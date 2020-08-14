d52be6b6d5a519883622c129cc165c2b60a5edaa
EnderUnknown <samkjell@yahoo.com>
2020-08-14 07:36:40 -0500

    Add files via upload

---
+++ Ep8/Scripts/TileSpawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSpawn : MonoBehaviour
+{
+    public GameObject[] tileSpawns;
+
+    void Start()
+    {
+        int r = Random.Range(0, tileSpawns.Length);
+        if (tileSpawns[r] != null)
+        {
+            GameObject go = Instantiate(tileSpawns[r], transform);
+            Generation.tileDict.Add(go.transform.position, go);
+        }
+    }
+}
